0b1b092f9a7bafd958871201b41ce066b509aead
BeatlesBOB <38206766+BeatlesBOB@users.noreply.github.com>
2022-02-16 21:24:21 +0100

    Update touch.js

--- tools/touch.js
+++ tools/touch.js
@@ -5,104 +5,90 @@ function findDistWall(kinect)
 {
     return new Promise((resolve, reject) => 
     {
-        if (kinect.open()) {
-            let distWall = [];    
-            kinect.on('bodyFrame', function(bodyFrame){
-                for(var i = 0;  i < bodyFrame.bodies.length; i++) 
-                {
-                    if (bodyFrame.bodies[i].tracked) 
-                    {
-                        distWall.push(bodyFrame.bodies[i].joints[3].cameraZ)
-                    }
+        let distWall = [];    
+        kinect.on('bodyFrame', function(bodyFrame){
+            for(var i = 0;  i < bodyFrame.bodies.length; i++) {
+                if (bodyFrame.bodies[i].tracked) {
+                    distWall.push(bodyFrame.bodies[i].joints[3].cameraZ)
                 }
-
-            });
-            
-            if(!isOpen){
-                isOpen = kinect.openBodyReader()
             }
 
-            setTimeout(function(){
-                if(distWall.length > 1)
-                {
-                    kinect.closeBodyReader().then(() =>{
-                        isOpen = false;
-                        resolve(numAverage(distWall));
-                    }) 
-                } else {
-                    findDistWall(kinect)
-                }
-            },5000);
-        } else {
-            console.log("Kinect could not be openend");
+        });
+            
+        if(!isOpen){
+            isOpen = kinect.openBodyReader()
         }
+
+        setTimeout(function(){
+            if(distWall.length > 0) {
+                kinect.closeBodyReader().then(()=>{
+                    isOpen = false;
+                    return resolve(numAverage(distWall));
+                });
+            } else {
+                findDistWall(kinect)
+                return reject()
+            }
+        },5000);
     });
 }
 
-function numAverage(a) 
-{
+function numAverage(a) {
     let b = a.length,
     c = 0, i;
-
-    for (i = 0; i < b; i++)
-    {
+    for (i = 0; i < b; i++){
       c += Number(a[i]);
     }
-
     return c/b;
 }
 
 module.exports = (io,kinect) => {
 
-    const touch = async function()
-    {
+    const touch = async function() {
         const socket = this;
-        console.log("Touch Opened");
-        avgDist = await findDistWall(kinect);
-        console.log(avgDist)
-        // if (kinect.open()) {
-            
-        //     kinect.on('bodyFrame',function (bodyFrame) {
-        //         let nb_peoples = 0;
-        //         let personnes = [];
-        //         let response;
+        if (kinect.open()) {
 
-        //         for(var i = 0;  i < bodyFrame.bodies.length; i++) {
-        //             if (bodyFrame.bodies[i].tracked && (bodyFrame.bodies[i].joints[7].cameraZ >= avgDist-offset  || bodyFrame.bodies[i].joints[11].cameraZ >= avgDist-offset)) {
-        //                 nb_peoples++
-        //                 response = {"nb_peoples": nb_peoples}
-        //                 let people = {}
-        //                 if(bodyFrame.bodies[i].joints[7].cameraZ >= avgDist-offset){
-        //                     people["left_hand"] = {
-        //                         "x": bodyFrame.bodies[i].joints[7].colorX,
-        //                         "y": bodyFrame.bodies[i].joints[7].colorY,
-        //                     }
-        //                 }
+            avgDist = await findDistWall(kinect);
+            kinect.on('bodyFrame',function (bodyFrame) {
+                let nb_peoples = 0;
+                let personnes = [];
+                let response;
 
-        //                 if(bodyFrame.bodies[i].joints[11].cameraZ >= avgDist-offset){
-        //                     people["right_hand"] = {
-        //                         "x": bodyFrame.bodies[i].joints[11].colorX,
-        //                         "y": bodyFrame.bodies[i].joints[11].colorY,
-        //                     }
-        //                 }
-        //                 personnes.push(people)
-        //             }
-        //         }
+                for(var i = 0;  i < bodyFrame.bodies.length; i++) {
+                    if (bodyFrame.bodies[i].tracked && (bodyFrame.bodies[i].joints[7].cameraZ >= avgDist-offset  || bodyFrame.bodies[i].joints[11].cameraZ >= avgDist-offset)) {
+                        nb_peoples++
+                        response = {"nb_peoples": nb_peoples}
+                        let people = {}
+                        if(bodyFrame.bodies[i].joints[7].cameraZ >= avgDist-offset){
+                            people["left_hand"] = {
+                                "x": bodyFrame.bodies[i].joints[7].colorX,
+                                "y": bodyFrame.bodies[i].joints[7].colorY,
+                            }
+                        }
 
-        //         response["peoples"] = personnes
+                        if(bodyFrame.bodies[i].joints[11].cameraZ >= avgDist-offset){
+                            people["right_hand"] = {
+                                "x": bodyFrame.bodies[i].joints[11].colorX,
+                                "y": bodyFrame.bodies[i].joints[11].colorY,
+                            }
+                        }
+                        personnes.push(people)
+                    }
+                }
 
-        //         if(response)  {
-        //             socket.emit('touch',response)
-        //         }
-        //     });
+                response["peoples"] = personnes
+
+                if(response)  {
+                    socket.emit('touch',response)
+                }
+            });
         
-        //     if(!isOpen) {
-        //         kinect.openBodyReader();
-        //     }
-       
-        // }  else {
-        //     console.log("Kinect could not be openend");
-        // }
+            if(!isOpen) {
+                kinect.openBodyReader();
+            }
+        } else {
+            console.log("Kinect could not be openend");
+        } 
     };
     return {touch}
 }
