df187510874a5442230a8cb64e80ab1d12b42cfd
BeatlesBOB <beatlesbob74120@gmail.com>
2022-03-09 11:26:01 +0100

    Update touch.js

--- tools/touch.js
+++ tools/touch.js
@@ -49,46 +49,43 @@ module.exports = (io,kinect) => {
         const socket = this;
         if (kinect.open()) {
             let avgDist
-            try{
-                avgDist= await findDistWall(kinect);                
-            }catch(e){
-                avgDist = await findDistWall(kinect);
-            }
-                have_dist = true;
-                kinect.on('bodyFrame',function (bodyFrame) {
-                    let nb_peoples = 0;
-                    let personnes = [];
-                    let response;
+            avgDist = await findDistWall(kinect).catch((e)=>{
+                console.log("fuck")
+            });                
 
-                    for(var i = 0;  i < bodyFrame.bodies.length; i++) {
-                        if (bodyFrame.bodies[i].tracked && (bodyFrame.bodies[i].joints[7].cameraZ >= avgDist-offset  || bodyFrame.bodies[i].joints[11].cameraZ >= avgDist-offset)) {
-                            nb_peoples++
-                            response = {"nb_peoples": nb_peoples}
-                            let people = {}
-                            if(bodyFrame.bodies[i].joints[7].cameraZ >= avgDist-offset){
-                                people["left_hand"] = {
-                                    "x": bodyFrame.bodies[i].joints[7].colorX,
-                                    "y": bodyFrame.bodies[i].joints[7].colorY,
-                                }
-                            }
+            // have_dist = true;
+            // kinect.on('bodyFrame',function (bodyFrame) {
+            //     let nb_peoples = 0;
+            //     let personnes = [];
+            //     let response;
+                    
+            //     for(var i = 0;  i < bodyFrame.bodies.length; i++) {
+            //         if (bodyFrame.bodies[i].tracked && (bodyFrame.bodies[i].joints[7].cameraZ >= avgDist-offset  || bodyFrame.bodies[i].joints[11].cameraZ >= avgDist-offset)) {
+            //             nb_peoples++
+            //             response = {"nb_peoples": nb_peoples}
+            //             let people = {}                            
+            //             if(bodyFrame.bodies[i].joints[7].cameraZ >= avgDist-offset){
+            //                 people["left_hand"] = {
+            //                     "x": bodyFrame.bodies[i].joints[7].colorX,
+            //                     "y": bodyFrame.bodies[i].joints[7].colorY,
+            //                 }
+            //             }
 
-                            if(bodyFrame.bodies[i].joints[11].cameraZ >= avgDist-offset){
-                                people["right_hand"] = {
-                                    "x": bodyFrame.bodies[i].joints[11].colorX,
-                                    "y": bodyFrame.bodies[i].joints[11].colorY,
-                                }
-                            }
-                            personnes.push(people)
-                        }
-                    }
-
-                    response["peoples"] = personnes
-
-                    if(response)  {
-                        socket.emit('touch',response)
-                    }
-                });
-                kinect.openBodyReader();
+            //             if(bodyFrame.bodies[i].joints[11].cameraZ >= avgDist-offset){
+            //                 people["right_hand"] = {
+            //                     "x": bodyFrame.bodies[i].joints[11].colorX,
+            //                     "y": bodyFrame.bodies[i].joints[11].colorY,
+            //                 }
+            //             }
+            //             personnes.push(people)
+            //         }
+            //     }
+            //     response["peoples"] = personnes
+            //     if(response)  {
+            //         socket.emit('touch',response)
+            //     }
+            // });
+            // kinect.openBodyReader();
         } else {
             console.log("Kinect could not be openend");
         } 
